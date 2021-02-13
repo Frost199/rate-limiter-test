97c9f5a4abdebde03db6a3901f78ffe283ea503f
Frost199 <emmaldini12@gmail.com>
2021-02-13 14:41:23 +0100

    feat: add rate limiting implementation to main file

--- app.py
+++ app.py
@@ -1,12 +1,38 @@
-from flask import Flask
+from flask import Flask, jsonify, Response
+from util.decorators import rate_limit
+from util.rate_limit import RateLimit
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.after_request
+def inject_x_rate_headers(response: Response) -> 'Response':
+    limit: RateLimit = RateLimit.get_view_rate_limit()
+    if limit and limit.send_headers:
+        header = response.headers
+        header.add('X-RateLimit-Limit', str(limit.limit))
+        header.add('X-RateLimit-Remaining', str(limit.remaining))
+        header.add('X-RateLimit-Reset', str(limit.reset))
+    return response
+
+
+@app.route('/', methods=['GET'])
+@rate_limit(limit=4, per=30, send_x_headers=True)
+def page_one():
+    return jsonify({
+        "success": "200",
+        "data": "Hello User"
+    }), 200
+
+
+@app.route('/second-page', methods=['GET'])
+@rate_limit(limit=4, per=30, send_x_headers=True)
+def page_two():
+    return jsonify({
+        "success": "200",
+        "data": "Hello User, Welcome to page 2"
+    }), 200
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', debug=True)
